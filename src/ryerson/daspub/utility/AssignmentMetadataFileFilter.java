2048aae64335964809e1306548d08824563bab38
Davis Marques <dmarq.ezz@gmail.com>
2012-01-19 17:35:36 -0500

    Updates to Course, Assignment, Submission model entities.

---
+++ src/ryerson/daspub/utility/AssignmentMetadataFileFilter.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2011 Davis Marques
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+package ryerson.daspub.utility;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import ryerson.daspub.Config;
+
+/**
+ * Assignment metadata file filter.
+ * @author dmarques
+ */
+public class AssignmentMetadataFileFilter implements FilenameFilter {
+
+    /**
+     * Determine if a file is an assignment metadata document.
+     * @param dir Parent directory
+     * @param name File name
+     * @return True if the file is an assignment metadata document, false otherwise.
+     */
+    public boolean accept(File dir, String name) {
+        if (name.toLowerCase().equals(Config.ASSIGNMENT_FILE_METADATA)) return true;
+        return false;
+    }
+
+} // end class
+
